650c209d7d1cfd3973e905a4e6ac4356cd31d55f
Amruth Pabba <legoamruth@me.com>
2016-06-26 23:05:52 -0500

    Update garage.py
    Made the file simpler.

--- Garage Solution/garage.py
+++ Garage Solution/garage.py
@@ -1,9 +1,11 @@
 import RPi.GPIO as GPIO
 from time import sleep
+#this is the pin variable, change it if your relay is on a different pin
+relay=26;
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(26, GPIO.OUT)
-GPIO.output(26, GPIO.HIGH)
-GPIO.output(26, GPIO.LOW)
+GPIO.setup(relay, GPIO.OUT)
+GPIO.output(relay, GPIO.HIGH)
+GPIO.output(relay, GPIO.LOW)
 sleep(1)
-GPIO.output(26, GPIO.HIGH)
+GPIO.output(relay, GPIO.HIGH)
 GPIO.cleanup()
