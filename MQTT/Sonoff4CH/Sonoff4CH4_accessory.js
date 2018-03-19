2bf8d5fbbc1b737d9fda8403a4938e0d7ee9e97f
Amruth Pabba <amruthpabba@gmail.com>
2018-03-19 13:23:46 -0500

    Sonoff 4CH(Pro)
    Files and script for the Sonoff 4CH(Pro). Initial release, will need to be tested for bugs,etc.

---
+++ MQTT/Sonoff4CH/Sonoff4CH4_accessory.js
@@ -0,0 +1,91 @@
+var Accessory = require('../').Accessory;
+var Service = require('../').Service;
+var Characteristic = require('../').Characteristic;
+var uuid = require('../').uuid;
+var mqtt = require('mqtt');
+var MQTT_IP = 'localhost' //change this if your MQTT broker is different
+var mqttMSG = false;
+
+
+var name = "Sonoff Dual Outlet2"; //accessory name
+var sonoffUUID = "hap-nodejs:accessories:sonoffdual" + name; //change this to your preferences
+var sonoffUsername = "2D:2D:31:4E:5A:F2";
+var MQTT_NAME = 'sonoffdual' //MQTT topic that was set on the Sonoff firmware
+
+
+var options = {
+  port: 1883,
+  host: MQTT_IP,
+//  username: 'pi', //enable only if you have authentication on your MQTT broker
+//  password: 'raspberry', //enable only if you have authentication on your MQTT broker
+  clientId: MQTT_NAME+'SonoffDualHAP2'
+};
+var sonoffTopic = 'cmnd/'+MQTT_NAME+'/power2';
+var client = mqtt.connect(options);
+
+client.on('message', function(topic, message) {
+//  console.log(message.toString());
+  message = message.toString();
+  mqttMSG = true;
+  if (message.includes('ON')){
+    sonoffObject.powerOn = true;
+  }
+  else{
+    sonoffObject.powerOn = false;
+  }
+  sonoff
+    .getService(Service.Outlet)
+    .setCharacteristic(Characteristic.On,sonoffObject.powerOn);
+});
+
+client.on('connect', function () {
+  client.subscribe('stat/'+MQTT_NAME+'/POWER')
+});
+
+var sonoffObject = {
+  powerOn: false,
+  setPowerOn: function(on) {
+    sonoffObject.powerOn = on;
+    if (on) {
+      client.publish(sonoffTopic, 'on');
+    } else {
+      client.publish(sonoffTopic, 'off');
+    }
+  },
+  identify: function() {
+    console.log(name + " Identified!");
+  }
+}
+
+var sonoff = exports.accessory = new Accessory(name, uuid.generate(sonoffUUID));
+
+sonoff.username = sonoffUsername;
+sonoff.pincode = "031-45-154";
+
+// listen for the "identify" event for this Accessory
+sonoff.on('identify', function(paired, callback) {
+  sonoffObject.identify();
+  callback();
+});
+
+sonoff
+  .addService(Service.Outlet, name)
+  .getCharacteristic(Characteristic.On)
+  .on('set', function(value, callback) {
+    if(mqttMSG){
+      mqttMSG = false;
+      callback();
+    }
+    else {
+      sonoffObject.setPowerOn(value);
+      callback();
+    }
+  });
+
+sonoff
+  .getService(Service.Outlet)
+  .getCharacteristic(Characteristic.On)
+  .on('get', function(callback) {
+    client.publish(sonoffTopic,'')
+    callback(undefined, sonoffObject.powerOn);
+  });
